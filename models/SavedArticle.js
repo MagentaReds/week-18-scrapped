81ff96b37ef2e894c79fe2f31519c1e7eb791517
MagentaReds <grantodaniels@gmail.com>
2017-05-25 01:55:27 -0500

    First versions of models

---
+++ models/SavedArticle.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var SavedArticleSchema = new Schema({
+  article: {
+    type: Schema.Types.ObjectId,
+    ref: "Article",
+    required: true
+  },
+  notes: [{
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }]
+});
+
+SavedArticleSchema.pre('remove', function(next) {
+    // Remove all the note documents that are attached to this savedarticle before removing this savedarticle
+    this.model('Note').remove({ _id: {$in: this.notes }}, next);
+});
+
+var SavedArticle = mongoose.model("SavedArticle", SavedArticleSchema);
+
+module.exports = SavedArticle;
